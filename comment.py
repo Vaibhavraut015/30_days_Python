64ce99c4285419fe7212a9095c1e6354f11cd09b
Vaibhav Mahadeo Raut <123192741+Vaibhavraut015@users.noreply.github.com>
2025-01-03 23:29:51 +0530

    Add files via upload

---
+++ comment.py
@@ -0,0 +1,22 @@
+# In python there are two types of comments 
+
+# single Line comment and Multiline comment
+
+#single line we use "#" symbol before text
+# Multiline comment we use "''''''" tripal semicolan or doubl colon
+
+
+#-----------------------example----------------------------
+
+#Addition of number one and number two is given below by arithmatic operator
+
+number1 = 10
+number2 = 20
+
+a=number1+number2
+
+print(a)
+
+'''answer is given
+below it is multiline comment
+'''
